5ad45b4b4f13aefb86683c090dc8e2774680a289
J00Ni <89300933+J00Ni@users.noreply.github.com>
2021-08-21 15:02:40 +0200

    Update login_event.py

--- login_event.py
+++ login_event.py
@@ -10,11 +10,4 @@ bot.remove_command('help')
 async def on_ready():
     print("Ich habe mich eingeloggt. Beep bop.")
 
-@bot.event
-async def on_message(message):
-    if message.channel.id == DEINE channel ID:
-        await message.add_reaction('emajio')
-        await message.add_reaction('emajio')
-
-
 bot.run (TOKEN, bot=True)
