6d06b186c358e81dcf73a3f3db3fc80c9cf24a66
J00Ni <89300933+J00Ni@users.noreply.github.com>
2021-08-21 13:27:54 +0200

    Update login_event.py

--- login_event.py
+++ login_event.py
@@ -1,1 +1,22 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
 
+TOKEN = 'DEIN TOKEN'
+bot = commands.Bot(command_prefix='+')
+bot.remove_command('help')
+
+
+@bot.event
+# Einloggen
+async def on_ready():
+    print("Ich habe mich eingeloggt. Beep bop.")
+
+@bot.event
+async def on_message(message):
+    if message.channel.id == DEINE channel ID:
+        await message.add_reaction('emajio')
+        await message.add_reaction('emajio')
+
+
+bot.run (TOKEN, bot=True)
